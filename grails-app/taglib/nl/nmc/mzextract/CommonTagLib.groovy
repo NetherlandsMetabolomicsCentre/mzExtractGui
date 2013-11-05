09290f7db10b43838beab1119c1c6473d78cebde
Michael <Michael@10.1.0.172>
2013-11-05 16:15:36 +0100

    Fix buttons. Right order and changed the icon

--- grails-app/taglib/nl/nmc/mzextract/CommonTagLib.groovy
+++ grails-app/taglib/nl/nmc/mzextract/CommonTagLib.groovy
@@ -29,9 +29,9 @@ class CommonTagLib {
 
         out << '<div style="padding-bottom: 5px; border-bottom:thin solid #cdcdcd;">'
         out << runExtractButton(dataFolder: dataFolder, extractFolder: extractFolder)
+        out << stopExtractButton(dataFolder: dataFolder, extractFolder: extractFolder)                
         out << settingsExtractButton(dataFolder: dataFolder, extractFolder: extractFolder)
         out << deleteExtractButton(dataFolder: dataFolder, extractFolder: extractFolder)
-        out << stopExtractButton(dataFolder: dataFolder, extractFolder: extractFolder)        
         out << extractStatus(dataFolderKey: dataFolder.key, extractFolderKey: extractFolder.key)        
         out << '</div>'
     }
@@ -209,7 +209,7 @@ class CommonTagLib {
         def dataFolder = attrs.dataFolder
         def extractFolder = attrs.extractFolder
         def currentStatus = extractService.readStatus(dataFolder.key, extractFolder.key)['status']
-        def buttonText = '<i class="icon-stop icon-white"></i> delete'
+        def buttonText = '<i class="icon-trash icon-white"></i> delete'
 
         if (!currentStatus || (currentStatus == 'new' || currentStatus == 'done')){
             out << g.link(controller:'extract', action:'delete', params:[submit_delete: 'true', dataFolderKey: dataFolder.key, extractFolderKey: extractFolder.key], alt:"Delete", class:"btn btn-mini btn-danger", onclick:"return confirm('Are you sure you want to delete this extraction?')") { buttonText }            
@@ -226,9 +226,9 @@ class CommonTagLib {
         def buttonText = '<i class="icon-stop icon-white"></i> stop'
 
         if (currentStatus == 'running'){
-            out << g.link(controller:'extract', action:'stop', params:[submit_stop: 'true', dataFolderKey: dataFolder.key, extractFolderKey: extractFolder.key], alt:"Stop", class:"btn btn-mini", onclick:"return confirm('Are you sure you want to stop this extraction of the remaining files?')") { buttonText }            
+            out << g.link(controller:'extract', action:'stop', params:[submit_stop: 'true', dataFolderKey: dataFolder.key, extractFolderKey: extractFolder.key], alt:"Stop", class:"btn btn-mini btn-danger", onclick:"return confirm('Are you sure you want to stop this extraction of the remaining files?')") { buttonText }            
         } else {
-            out << '<button class="btn btn-mini  btn-danger disabled">' + buttonText + '</button>'            
+            out << '<button class="btn btn-mini disabled">' + buttonText + '</button>'            
         }
     }
     
@@ -238,7 +238,7 @@ class CommonTagLib {
         def extractFolder = attrs.extractFolder
         def alignFolder = attrs.alignFolder
 
-        out << g.link(controller:'align', action:'delete', params:[submit_delete: 'true', dataFolderKey: dataFolder.key, extractFolderKey: extractFolder.key, alignFolderKey: alignFolder.key], alt:"Delete", class:"btn btn-mini btn-danger", onclick:"return confirm('Are you sure you want to delete this alignment?')") { '<i class="icon-stop icon-white"></i> delete' }
+        out << g.link(controller:'align', action:'delete', params:[submit_delete: 'true', dataFolderKey: dataFolder.key, extractFolderKey: extractFolder.key, alignFolderKey: alignFolder.key], alt:"Delete", class:"btn btn-mini btn-danger", onclick:"return confirm('Are you sure you want to delete this alignment?')") { '<i class="icon-trash icon-white"></i> delete' }
     }
 
     def deleteCombineButton = { attrs, body ->
@@ -247,7 +247,7 @@ class CommonTagLib {
         def extractFolder = attrs.extractFolder
         def alignFolder = attrs.alignFolder ?: null
         def combineFolder = attrs.combineFolder
-        def buttonText = '<i class="icon-stop icon-white"></i> delete'        
+        def buttonText = '<i class="icon-trash icon-white"></i> delete'        
 
         def currentStatus = combineService.readStatus(dataFolder.key, extractFolder.key, alignFolder?.key ?: null, combineFolder.key)['status']
         
