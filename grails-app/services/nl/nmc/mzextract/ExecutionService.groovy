dd9074986399f5d6b969b1f7f88d9533a6cc30a7
Michael van Vliet <github@michaelvanvliet.nl>
2013-11-26 11:22:17 +0100

    Added error to log

--- grails-app/services/nl/nmc/mzextract/ExecutionService.groovy
+++ grails-app/services/nl/nmc/mzextract/ExecutionService.groovy
@@ -25,9 +25,7 @@ class ExecutionService {
         arguments.each { argument -> command += "\"${argument}\" " }
 
         try {
-            
-            println command
-            
+
             // start the execution
             def proc = command.execute()
             proc.waitFor()
@@ -37,6 +35,7 @@ class ExecutionService {
         } catch (e) {
             log.error "execution failed of: ${command}"
             log.error "stderr: ${proc.err.text}"
+            log.error " due to: ${e}"
             response += "\n  --- stderr: ${proc.err.text}"
         }
 
